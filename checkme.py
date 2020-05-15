a0055c8274bce356a12f587d0e6e5f8427b51cc1
YoshiDesign <yoshidesigns@outlook.com>
2020-05-14 23:16:08 -0400

    made directory comparison

--- checkme.py
+++ checkme.py
@@ -3,7 +3,27 @@ from os import listdir
 
 def main():
 
-    
+    for s_name in listdir("SCR_1"):
+        check = s_name.split("_")
+        for i in range(len(check)):
+            if check[i] == "SCR.html":
+                check[i] = "TAB.html"
+        
+        find_me = "_".join(check)
+
+
+        for t_name in listdir("TAB_1"):
+            found = False
+
+            if t_name == find_me:
+                found = True
+                break
+        
+        if found:
+            found = False
+        else:
+            print(f"Failed to find {find_me}")
+
 
 if __name__ == "__main__":
     main()
